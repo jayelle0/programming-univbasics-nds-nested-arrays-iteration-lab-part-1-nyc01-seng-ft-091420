e76865d99030395406157c5d12f71859a65611f7
Jacqueline <lojacqueline2@gmail.com>
2020-09-03 19:09:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,8 +2,8 @@ def find_even_values(src)
 aoa = [ [10, 11], [99, 50, 3, 4], [23, 41] ]
 
   # Output all even values in each nested array
-count = 0 
-while count < aoa.size do 
+row = 0 
+while row < aoa.count do 
   inner_count = 0 
   while inner_count < aoa[count].size do
     if aoa[count][inner_count].even?
