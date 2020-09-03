2c26a68712b7e65d7e4dfa47c4ce39c8d19273f1
Jacqueline <lojacqueline2@gmail.com>
2020-09-03 19:11:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,13 +4,13 @@ aoa = [ [10, 11], [99, 50, 3, 4], [23, 41] ]
   # Output all even values in each nested array
 row = 0 
 while row < aoa.count do 
-  inner_count = 0 
-  while inner_count < aoa[count].size do
-    if aoa[count][inner_count].even?
-      p aoa[count][inner_count]
-      inner_count += 1
+  element = 0 
+  while element < src[row].count do
+    if src[row][element].even?
+      p src[row][element]
+      element += 1
     end
-    count += 1
+    row += 1
   end
 end
 end
